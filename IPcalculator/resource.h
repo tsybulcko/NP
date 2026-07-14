df9d8aa7b15a07be69d6eca7656b9c66ef4f5b80
roman <roman@ROMANPC>
2026-07-14 22:16:52 +0300

    DialogBox DONE

--- IPcalculator/resource.h
+++ IPcalculator/resource.h
@@ -6,8 +6,11 @@
 #define IDC_STATIC_IP_ADDRESS           1001
 #define IDC_STATIC_IP_MASK              1002
 #define IDC_IP_ADDRESS                  1003
-#define IDC_IPADDRESS2                  1004
 #define IDC_IP_MASK                     1004
+#define IDC_EDIT_PREFIX                 1005
+#define IDC_SPIN_PREFIX                 1020
+#define IDC_STATIC_INFO                 1021
+#define IDC_STATIC_FRAME                1021
 
 // Next default values for new objects
 // 
@@ -15,7 +18,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        103
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1005
+#define _APS_NEXT_CONTROL_VALUE         1022
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif

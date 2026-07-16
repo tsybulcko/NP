c1ef0eac69efd75ecaec382bb83ca9a0019c0304
roman <roman@ROMANPC>
2026-07-16 22:27:52 +0300

    NP

--- IPcalculator/main.cpp
+++ IPcalculator/main.cpp
@@ -1,7 +1,42 @@
 #include<Windows.h>
+#include"resource.h"
+
+BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
 INT WINAPI WinMain(HINSTANCE hInatance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow)
 {
-
+	DialogBoxParam(hInatance, MAKEINTRESOURCE(IDD_DIALOG_MAIN), NULL, (DLGPROC)DlgProc, NULL);
 	return 0;
+}
+
+BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch (uMsg)
+	{
+	case WM_INITDIALOG:
+		break;
+	case WM_COMMAND:
+	{
+		switch (LOWORD(wParam))
+		{
+		case IDC_IP_ADDRESS:
+		{
+			DWORD dwIPaddress = 0;
+			DWORD dwIPmask = 0;
+			HWND hIPaddress = GetDlgItem(hwnd, IDC_IP_ADDRESS)
+
+		}
+			break;
+		case IDOK:
+			break;
+		case IDCANCEL: EndDialog(hwnd, 0);
+		}
+	}
+	break;
+	case WM_CLOSE: EndDialog(hwnd, 0);
+	}
+	return FALSE;
+
+
+
 }
